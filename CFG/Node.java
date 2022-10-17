26b3b73d18e6492209f97ce250f8d4b11369d8cb
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-10-17 19:14:09 +0600

    cfg done

---
+++ CFG/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cfg;
+
+import java.util.ArrayList;
+
+public class Node {
+    int nodeNumber;
+    String Statement;
+    ArrayList<Node>parents = new ArrayList<>();
+    ArrayList<Node>childs = new ArrayList<>();
+    
+    public Node(int nodeNo, String nodeStatement){
+        this.nodeNumber = nodeNo;
+        this.Statement = nodeStatement;
+    }
+}
